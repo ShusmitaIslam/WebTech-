4bb04601e7613758ba145504b97295d003f82188
Shusmita Islam <57425572+ShusmitaIslam@users.noreply.github.com>
2021-03-03 23:04:53 +0600

    Add files via upload

---
+++ lab 4.php
@@ -0,0 +1,323 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form>
+<fieldset style="width: 200px; ">
+<img src="student.jpg" alt="student" width="50" height="60">  <p><h2>STUDENT</h2></p>
+<pre><a href="">Home</a>|<a href="">Login</a>|<a href="">Registration</a></pre>               
+<hr style="border: 0.1px solid;">
+<pre>                               
+
+
+<b>WELCOME TO STUDENT</b>
+
+
+</pre>
+</fieldset>
+
+</form>
+
+</body?
+</html>
+
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<form>
+<fieldset style="width: 200px; ">
+<img src="student.jpg" alt="student" width="50" height="60">  <p><h2>STUDENT</h2></p>
+<pre><a href="">Home</a>|<a href="">Login</a>|<a href="">Registration</a></pre>               
+<hr style="border: 0.1px solid;">
+
+
+<fieldset style="width: 300px; "> 
+<legend><h3><b>REGISTRATION</b></h3></legend> 
+<pre><b>Name             :</b> <input type="text" name="name">
+<hr style="border: 0.1px solid;">
+<b>E-mail           :</b> <input type="text" name="email">
+<hr style="border: 0.1px solid;">
+<pre><b>User Name        : </b><input type="text" name="name">
+<hr style="border: 0.1px solid;">
+<b>Password         : </b><input type="text" name="password">
+<hr style="border: 0.1px solid;">
+<b>Confirm Password : </b><input type="text" name="confirm password"><br></pre>
+<hr style="border: 0.1px solid;">
+<fieldset style="width: 300px; height:60 ">
+<legend><h3><b>GENDER</b></h3></legend> 
+<p><input type="radio" name="gender" value="female">Female <input type="radio" name="gender" value="male">Male <input type="radio" name="gender" value="other">Other</p>
+</fieldset>
+<hr style="border: 0.1px solid;">
+<fieldset style="width: 300px; height:60">
+<legend><h3><b>Date of Birth</b></h3></legend>  
+<pre><input type="text" name="dd">/<input type="text" name="mm">/<input type="text" name="yy">(dd/mm/yyyy)</pre>
+</fieldset>
+<hr style="border: 0.1px solid;">
+<p><input type="submit" name="submit" value="Submit">  <input type="button" value="Reset"></p> 
+</fieldset>
+
+</fieldset>
+
+</form>
+
+</body?
+</html>
+
+
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<form>
+<fieldset style="width: 200px; ">
+<img src="student.jpg" alt="student" width="50" height="60">  <p><h2>STUDENT</h2></p>
+<pre><a href="">Home</a>|<a href="">Login</a>|<a href="">Registration</a></pre>               
+<hr style="border: 0.1px solid;">
+
+
+<fieldset style="width: 300px; ">
+<legend><h3><b>LOGIN</b></h3></legend>
+<pre><b>User Name: </b><input type="text" name="name"><br>
+<b>Password : </b><input type="text" name="password"><br></pre>
+<hr style="border: 0.1px solid;">
+<input type="checkbox" id="Remember Me" name="Remember Me[]" value="Remember Me"> Remember Me
+<p><input type="submit"><h4 style="Color: blue">Forgot Password?</h4></p>
+</fieldset>
+</fieldset>
+</form>
+
+</body?
+</html>
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<form>
+<fieldset style="width: 200px; ">
+<img src="student.jpg" alt="student" width="50" height="60">  <p><h2>STUDENT</h2></p>
+<pre><a href="">Home</a>|<a href="">Login</a>|<a href="">Registration</a></pre>               
+<hr style="border: 0.1px solid;">
+
+
+<fieldset style="width: 300px; ">
+<legend><h3><b>FORGOT PASSWORD</b></h3></legend>
+<pre><b>Enter Email: </b><input type="text" name="name"><br>
+<hr style="border: 0.1px solid;">
+<p><input type="submit">
+</fieldset>
+</fieldset>
+</form>
+
+</body?
+</html>
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<form>
+<fieldset style="width: 200px; ">
+<img src="student.jpg" alt="student" width="50" height="60">  <p><h2>STUDENT</h2></p>
+<pre>                                                     Logged in as <a href="">Bob</a>|<a href="">Logout</a></pre>               
+<hr style="border: 0.1px solid;"> 
+<pre>                                             <b>WELCOME BOb</b></pre>
+<fieldset style="width: 160px; ">
+<pre><b>Account</b><br>
+<hr style="border: 0.1px solid;">
+<pre><b>*</b> <a href=""><u>Dashboard</u></a>
+<b>*</b> <a href=""><u>View Profile</u></a>
+<b>*</b> <a href=""><u>Edit Profile</u></a>                 
+<b>*</b> <a href=""><u>Change Profile Picture</u></a>
+<b>*</b> <a href=""><u>Change Password</u></a>
+<b>*</b> <a href=""><u>Logout</u></a>
+</fieldset>
+</fieldset>
+</form>
+
+</body?
+</html>
+
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<form>
+<fieldset style="width: 200px; ">
+<img src="student.jpg" alt="student" width="50" height="60">  <p><h2>STUDENT</h2></p>
+<pre>                                                     Logged in as <a href="">Bob</a>|<a href="">Logout</a></pre>               
+<hr style="border: 0.1px solid;"> 
+<fieldset style="width: 160px; ">
+<pre><b>Account</b><br>
+<hr style="border: 0.1px solid;">
+<pre><b>*</b> <a href=""><u>Dashboard</u></a>
+<b>*</b> <a href=""><u>View Profile</u></a>
+<b>*</b> <a href=""><u>Edit Profile</u></a>                 
+<b>*</b> <a href=""><u>Change Profile Picture</u></a>
+<b>*</b> <a href=""><u>Change Password</u></a>
+<b>*</b> <a href=""><u>Logout</u></a>
+</fieldset>
+
+<fieldset style="width: 300px; "> 
+<legend><h3><b>PROFILE</b></h3></legend> 
+<pre><img src="woman logo.jpg" alt="student" width="50" height="60">
+            <a href=""><u>Change</u></a>
+<pre><b>Name             :</b> <input type="text" name="name">
+<hr style="border: 0.1px solid;">
+<b>E-mail           :</b> <input type="text" name="email">
+<hr style="border: 0.1px solid;">
+<pre><b>Gender             :</b> <input type="text" name="name">
+<hr style="border: 0.1px solid;">
+<pre><b>Date of Birth          :</b> <input type="text" name="name">
+<hr style="border: 0.1px solid;">
+<a href=""><u>Edit Profile</u></a>
+</pre>
+</fieldset>
+</fieldset>
+</form>
+
+</body?
+</html>
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<form>
+<fieldset style="width: 200px; ">
+<img src="student.jpg" alt="student" width="50" height="60">  <p><h2>STUDENT</h2></p>
+<pre>                                                     Logged in as <a href="">Bob</a>|<a href="">Logout</a></pre>               
+<hr style="border: 0.1px solid;"> 
+<fieldset style="width: 160px; ">
+<pre><b>Account</b><br>
+<hr style="border: 0.1px solid;">
+<pre><b>*</b> <a href=""><u>Dashboard</u></a>
+<b>*</b> <a href=""><u>View Profile</u></a>
+<b>*</b> <a href=""><u>Edit Profile</u></a>                 
+<b>*</b> <a href=""><u>Change Profile Picture</u></a>
+<b>*</b> <a href=""><u>Change Password</u></a>
+<b>*</b> <a href=""><u>Logout</u></a>
+</fieldset>
+
+<fieldset style="width: 300px; "> 
+<legend><h3><b>EDIT PROFILE</b></h3></legend> 
+<pre><b>Name             :</b> <input type="text" name="name">
+<hr style="border: 0.1px solid;">
+<b>E-mail           :</b> <input type="text" name="email"></pre>
+<hr style="border: 0.1px solid;">
+<pre><b>GENDER</b>           :<input type="radio" name="gender"value="female">Female <input type="radio" name="gender" value="male">Male <input type="radio" name="gender" value="other">Other</pre>
+<hr style="border: 0.1px solid;">
+<pre><b>Date of Birth    :</b> <input type="text" name="name">
+                       (dd/mm/yyyy)
+<hr style="border: 0.1px solid;">
+<input type="Submit">
+</pre>
+</fieldset>
+</fieldset>
+</form>
+
+</body?
+</html>
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<form>
+<fieldset style="width: 200px; ">
+<img src="student.jpg" alt="student" width="50" height="60">  <p><h2>STUDENT</h2></p>
+<pre>                                                     Logged in as <a href="">Bob</a>|<a href="">Logout</a></pre>               
+<hr style="border: 0.1px solid;"> 
+<fieldset style="width: 160px; ">
+<pre><b>Account</b><br>
+<hr style="border: 0.1px solid;">
+<pre><b>*</b> <a href=""><u>Dashboard</u></a>
+<b>*</b> <a href=""><u>View Profile</u></a>
+<b>*</b> <a href=""><u>Edit Profile</u></a>                 
+<b>*</b> <a href=""><u>Change Profile Picture</u></a>
+<b>*</b> <a href=""><u>Change Password</u></a>
+<b>*</b> <a href=""><u>Logout</u></a>
+</fieldset>
+
+<fieldset style="width: 300px; ">
+<legend><h3><b>Profile Picture</b></h3></legend>
+<img src="female logo.jpg" alt=" " width="200" height="200">
+<input type="file" name="file"><br/>
+<hr style="border: 0.1px solid;">
+<input type="submit" value="Upload" name="Submit1"> 
+</fieldset>
+</fieldset>
+</form>
+
+</body?
+</html>
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<form>
+<fieldset style="width: 200px; ">
+<img src="student.jpg" alt="student" width="50" height="60">  <p><h2>STUDENT</h2></p>
+<pre>                                                     Logged in as <a href="">Bob</a>|<a href="">Logout</a></pre>               
+<hr style="border: 0.1px solid;"> 
+<fieldset style="width: 160px; ">
+<pre><b>Account</b><br>
+<hr style="border: 0.1px solid;">
+<pre><b>*</b> <a href=""><u>Dashboard</u></a>
+<b>*</b> <a href=""><u>View Profile</u></a>
+<b>*</b> <a href=""><u>Edit Profile</u></a>                 
+<b>*</b> <a href=""><u>Change Profile Picture</u></a>
+<b>*</b> <a href=""><u>Change Password</u></a>
+<b>*</b> <a href=""><u>Logout</u></a>
+</fieldset>
+
+<fieldset style="width: 300px; ">
+<legend><h3><b>CHANGE PASSWORD</b></h3></legend>
+<pre><b>Current Password    : </b><input type="text" name="name"><br>
+<b style="Color: green">New Password        : </b><input type="text" name="New Password"><br>
+<b style="Color: red">Retype New Password : </b><input type="text" name="Retype New Password"><br></pre>
+<hr style="border: 0.1px solid;">
+<input type="submit">
+</fieldset>
+</fieldset>
+</form>
+
+</body?
+</html>
