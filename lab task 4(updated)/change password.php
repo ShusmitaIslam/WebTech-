480c574fa80651bc453c899c833fae90ce1a58de
Shusmita Islam <57425572+ShusmitaIslam@users.noreply.github.com>
2021-03-17 23:50:14 +0600

    Add files via upload

---
+++ lab task 4(updated)/change password.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Password Change</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+/* Style the header */
+header {
+  background-color: #666;
+  padding: 30px;
+  text-align: left;
+  font-size: 35px;
+  color: white;
+}
+article {
+  float: Center;
+  padding: 270px;
+  text-align: Center;
+  height: 300px; /* only for demonstration, should be removed */
+}
+/* Clear floats after the columns */
+section::after {
+  content: "";
+  display: table;
+  text-align: Center;
+  clear: both;
+}
+
+}
+</style>
+</head>
+<body>
+<h3 align="center">CHANGE PASSWORD</h3>
+<div><?php if(isset($message)) { echo $message; } ?></div>
+<form method="post" action="" align="center">
+Current Password:<br>
+<input type="password" name="currentPassword"><span id="currentPassword" class="required"></span>
+<br>
+New Password:<br>
+<input type="password" name="newPassword"><span id="newPassword" class="required"></span>
+<br>
+Confirm Password:<br>
+<input type="password" name="confirmPassword"><span id="confirmPassword" class="required"></span>
+<br><br>
+<input type="submit">
+</form>
+<br>=
+<br>
+</body>
+</html>
